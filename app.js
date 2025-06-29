c2954dcc3d07ff6f3274d1bbe178f73e9c9aeaec
nickc5115 <nicholas.a.carfagno@gmail.com>
2025-06-29 19:08:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require('dotenv').config({ silent: true })
+  console.log("Starting Netlify CMS OAuth provider...");
+  console.log("process.env.PORT at startup:", process.env.PORT);
 const express = require('express')
 const middleWarez = require('./index.js')
 const port = process.env.PORT
